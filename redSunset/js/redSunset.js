7e8865920ee305426ce64d984354845ca321bf0a
ezemoralesmdp <ezemoralesmdp@gmail.com>
2021-07-28 16:37:25 -0300

    Last update

--- redSunset/js/redSunset.js
+++ redSunset/js/redSunset.js
@@ -1,4 +1,5 @@
 /* tab */
+
 openPersonalData(event, "Overview")
 function openPersonalData(evt, openPersonalData) {
     var i, section__tabcontent, section__tablinks;
@@ -10,6 +11,45 @@ function openPersonalData(evt, openPersonalData) {
     for (i = 0; i < section__tablinks.length; i++) {
         section__tablinks[i].className = section__tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(openPersonalData).style.display = "block";
-    evt.currentTarget.className += " active";
+
+    var div__personalData = document.getElementById(openPersonalData);
+
+    if (div__personalData)
+    {
+        div__personalData.style.display = "block";
+    }
+
+    if (evt)
+    {
+        evt.currentTarget.className += " active";
+    }
+}
+
+/* gallery */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
